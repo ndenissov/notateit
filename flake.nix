2a30ed44ef54ffaf7aabb088864455339749e09e
Nikita Denissov <n.denissov@proton.me>
2026-07-20 15:55:29 +0500

    nix: update flake.nix with rename and QT_PLUGIN_PATH fix

--- flake.nix
+++ flake.nix
@@ -1,10 +1,8 @@
 {
   description = "Notateit Viewer Remake";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
-
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
@@ -17,32 +15,27 @@
       ];
     in {
       packages.${system}.default = pythonPackages.buildPythonApplication {
-        pname = "notateit-viewer";
+        pname = "notateit";
         version = "1.0.2";
         pyproject = true;
-
         src = ./.;
-
         nativeBuildInputs = [
           pythonPackages.poetry-core
           pythonPackages.setuptools
           pkgs.qt6.wrapQtAppsHook
         ];
-
         buildInputs = [
           pkgs.qt6.qtbase
           pkgs.qt6.qtwayland
         ];
-
         propagatedBuildInputs = [
           pythonPackages.pyside6
           pythonPackages.pillow
         ];
-
         postInstall = ''
           mkdir -p $out/share/applications
           mkdir -p $out/share/icons/hicolor/512x512/apps
-          
+
           if [ -d AppDir ]; then
             [ -f AppDir/usr/share/applications/notateit.viewer.remake.desktop ] && \
               cp AppDir/usr/share/applications/notateit.viewer.remake.desktop $out/share/applications/
@@ -50,27 +43,24 @@
               cp AppDir/usr/share/hicolor/512x512/apps/notateit_remake.png $out/share/icons/hicolor/512x512/apps/
           fi
         '';
-
         meta = with pkgs.lib; {
           description = "Notateit Viewer Remake";
           platforms = platforms.linux;
-          mainProgram = "notateit-viewer";
+          mainProgram = "notateit";
         };
-
         qtWrapperArgs = [
           "--set QT_QPA_PLATFORM wayland;xcb"
         ];
-
       };
-
       devShells.${system}.default = pkgs.mkShell {
           buildInputs = [
             pkgs.poetry
             pkgs.python313
           ];
-
-          shellHook = ''export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeLibs}:$LD_LIBRARY_PATH"'';
+          shellHook = ''
+          export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeLibs}:$LD_LIBRARY_PATH"
+          export QT_PLUGIN_PATH=""
+          '';
         };
-
     };
 }
